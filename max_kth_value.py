e869376be5c75a9488568c6dcd2bbfa169fd7e30
domGitDev <silvadom2014@gmail.com>
2017-05-21 23:38:42 +0000

    Update max_kth_value.py
    Validate param type

--- max_kth_value.py
+++ max_kth_value.py
@@ -3,11 +3,20 @@ import heapq
 
 def find_max_kth_value(arr, kth):
 	""" Function to find kth maximum value
-	in an unsorted array. 1 <= k <= n
-	param arr: list of values
-	param kth: int, denote 1st, 2nd, nth max value
-	return: kth max value
+		in an unsorted array. 1 <= k <= n
+		param arr: list of values
+		param kth: int, denote 1st, 2nd, nth max value
+		return: kth max value
 	"""
+	def validate():
+		if not isinstance(arr, list):
+			raise TypeError(
+				'Expected type list, got %s.' % type(arr))
+		if not isinstance(kth, int):
+			raise TypeError(
+				'Expected type list, got %s.' % type(kth))
+				
+	validate()			
 	min_heap = []
 	for i, val in enumerate(arr):
 		if i < kth:
@@ -16,7 +25,10 @@ def find_max_kth_value(arr, kth):
 		if val > min_heap[0]:
 			heapq.heappop(min_heap)
 			heapq.heappush(min_heap, val)
-	return min_heap[0]
+	if not min_hip:
+		return None
+	return min_heap[0]	
+
 
 
 

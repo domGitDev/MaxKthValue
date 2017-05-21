5b4e662f7fcc79a07aae6f7a11bb4eec0f726594
domGitDev <silvadom2014@gmail.com>
2017-05-21 23:22:16 +0000

    Update max_kth_value.py
    Rename function name to find_max_kth_value

--- max_kth_value.py
+++ max_kth_value.py
@@ -1,8 +1,13 @@
 import heapq
 
 
-def max_kth_value(arr, kth):
-	
+def find_max_kth_value(arr, kth):
+	""" Function to find kth maximum value
+	in an unsorted array. 1 <= k <= n
+	param arr: list of values
+	param kth: int, denote 1st, 2nd, nth max value
+	return: kth max value
+	"""
 	min_heap = []
 	for i, val in enumerate(arr):
 		if i < kth:
@@ -14,6 +19,6 @@ def max_kth_value(arr, kth):
 	return min_heap[0]
 
 
-data = [2, 1, 3, 1, 2]
-print max_kth_value(data, 1)
+
+
 
